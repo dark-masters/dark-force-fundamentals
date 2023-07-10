d2a17d89d1372e5aafd0e9031522d043f6be87e2
Ktoxcon <kennethtoxro@gmail.com>
2023-07-09 22:22:14 -0600

    feat: javascript fundamentals class

---
+++ js/basics.js
@@ -0,0 +1,29 @@
+// Guessing Game
+
+/**
+ * 1. Generate a random number (1-10)
+ * 2. Process user input
+ * 3. Compare generated number and user input
+ * 4. Let the user know if the number is greater/less than the random number
+ * 5. If user input is equal to the number, user wins!
+ */
+
+// Additional Features:
+/**
+ *
+ * 0. Clone the repo and make sure code is up to date
+ *
+ * 1. Replicate code seen in class
+ *
+ * 2. Enable Game History
+ *    a. Display an unordered list with the latest game results at the bottom of the page.
+ *
+ * 3. Add a non-negative number contraint in user input
+ *    a. Try to do it in HTML (Non-mandatory)
+ *
+ * 4. Enable another user input to let the user specify the guessing number range
+ *
+ * 5. Create a PR (Pull request) in github and mark Kenneth as reviewer
+ *    a. Wait for feedback and comments and participate in code review discussion.
+ *
+ */
